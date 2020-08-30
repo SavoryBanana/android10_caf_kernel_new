95a4b582defc28c4a3bfdbb2c7fe9cc0b6a7425b
Batu33-TR <batuhanarslanoglu4@gmail.com>
2020-08-30 14:47:35 +0000

    mm: page-writeback.c: Clear cherry-pick lines

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -92,15 +92,11 @@ int vm_highmem_is_dirtyable;
 /*
  * The generator of dirty data starts writeback at this percentage
  */
-<<<<<<< HEAD
-int vm_dirty_ratio = 20;
-=======
 #ifdef CONFIG_ZEN_INTERACTIVE
 int vm_dirty_ratio = 30;
 #else
 int vm_dirty_ratio = 20;
 #endif
->>>>>>> 79c6094d831a... [BACKPORT] Implement zen-tune v4.5
 
 /*
  * vm_dirty_bytes starts at 0 (disabled) so that it is a function of
